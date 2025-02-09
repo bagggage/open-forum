b346dda447daf1f8b4d00e25a0136425a125a778
Christmas <slinko.art@mail.ru>
2025-02-09 19:50:53 +0300

    added category router for app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from src.api.v1 import question
+from src.api.v1 import category
 
 app = FastAPI()
 
 app.include_router(question.router)
+app.include_router(category.router)
