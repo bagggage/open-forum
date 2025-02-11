d7e2064cb78ef4aa77d3879a72a1378f436bd319
mxrpheus6 <pkazachenkoo@gmail.com>
2025-02-11 20:22:05 +0300

    implemented service for create answer method

---
+++ backend/src/service/answer.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.repositories.answer import create_answer
+from src.repositories.question import get_question_by_id
+from src.schemas.answer import AnswerCreate
+
+
+async def create_answer_service(db: AsyncSession, answer_data: AnswerCreate, user_id: int):
+    question = await get_question_by_id(db, answer_data.question_id)
+
+    if not question:
+        raise HTTPException(status_code=404, detail="Question not found")
+
+    return await create_answer(db, answer_data, user_id)
