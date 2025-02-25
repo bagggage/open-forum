80dc27e3fb9b51007224e43e3d638f7e27017f6e
Christmas <slinko.art@mail.ru>
2025-02-25 15:46:51 +0300

    implemented integration tests for tags

---
+++ backend/src/tests/integration/test_tag.py
@@ -0,0 +1,41 @@
+import pytest
+
+class TestTag:
+    tag_id = None
+
+    @pytest.mark.asyncio
+    async def test_get_tags(self, async_client):
+        response = await async_client.get("/v1/tags/")
+
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+        print(response.json())
+
+    @pytest.mark.asyncio
+    async def test_create_tag(self, async_client):
+        tag_data = {"name": "New Test Tag"}
+
+        response = await async_client.post(
+            "/v1/tags/",
+            json=tag_data
+        )
+
+        assert response.status_code == 200
+        created_tag= response.json()
+        TestTag.tag_id = created_tag["id"]
+
+    @pytest.mark.asyncio
+    async def test_get_tag(self, async_client):
+        assert TestTag.tag_id is not None
+
+        response = await async_client.get(f"/v1/tags/{TestTag.tag_id}")
+
+        assert response.status_code == 200
+
+    @pytest.mark.asyncio
+    async def test_delete_tag(self, async_client):
+        assert TestTag.tag_id is not None
+
+        response = await async_client.delete(f"/v1/tags/{TestTag.tag_id}")
+
+        assert response.status_code == 200
