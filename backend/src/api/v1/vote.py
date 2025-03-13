cbde7e242f118fef735ef78a827be31d4c018ad9
bagggage <pigulevskiykostya@gmail.com>
2025-03-13 21:00:59 +0300

    implemented router for getting all votes for answer

--- backend/src/api/v1/vote.py
+++ backend/src/api/v1/vote.py
@@ -6,6 +6,7 @@ from src.db.engine import get_async_session
 from src.service.vote import create_vote_service
 from src.service.vote import get_votes_service
 from src.service.vote import get_vote_service
+from src.service.vote import get_votes_for_answer_service
 from src.schemas.vote import VoteCreate
 from src.schemas.vote import VoteResponse
 from src.models import User
@@ -50,3 +51,14 @@ async def get_vote(
 ):
     return await get_vote_service(db, vote_id)
 
+@router.get(
+    "/by-answer/{answer_id}",
+    response_model=List[VoteResponse],
+    summary="Get a list of votes for answer by its ID"
+)
+async def get_votes_for_answer(
+    answer_id: int,
+    db: AsyncSession = Depends(get_async_session)
+):
+    votes = await get_votes_for_answer_service(db, answer_id)
+    return [VoteResponse.from_orm(v) for v in votes]
