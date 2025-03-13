9ad855f3b80348a3f4f7bc209e4c279793aadb86
Christmas <slinko.art@mail.ru>
2025-03-13 15:16:13 +0300

    implemented router for getting all votes

--- backend/src/api/v1/vote.py
+++ backend/src/api/v1/vote.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.utils.auth_dependencies import current_user
 from src.db.engine import get_async_session
 from src.service.vote import create_vote_service
+from src.service.vote import get_votes_service
 from src.schemas.vote import VoteCreate
 from src.schemas.vote import VoteResponse
 from src.models import User
+from typing import List
 
 router = APIRouter(prefix="/v1/votes", tags=["Votes"])
 
@@ -22,4 +24,17 @@ async def create_vote(
 ):
     vote = await create_vote_service(db, vote_data, user.id)
     return VoteResponse.from_orm(vote)
+
+@router.get(
+    "/", 
+    response_model=List[VoteResponse], 
+    summary="Get a list of all votes with pagination"
+)
+async def get_votes(
+    skip: int = 0,
+    limit: int = 10,
+    db: AsyncSession = Depends(get_async_session)
+):
+    votes = await get_votes_service(db, skip, limit)
+    return [VoteResponse.from_orm(v) for v in votes]
 
